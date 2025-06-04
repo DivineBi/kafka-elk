96f42e1433bed12d0a7da9b620adcef84e4e1c34
Divine-Bi <dbingila@outlook.com>
2025-06-04 09:46:52 +0200

    test producer $ curl -X POST http://localhost:8080/produce?content=MonMessageKafka MonMessageKafka message reçu par le consumer 2025-06-04T09:43:46.890+02:00  INFO 16260 --- [kafka] [ntainer#0-0-C-1] c.learn.kafka.consumer.MessageConsumer   : Message receive : MonMessageKafka

--- src/main/java/com/learn/kafka/consumer/MessageConsumer.java
+++ src/main/java/com/learn/kafka/consumer/MessageConsumer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MessageConsumer {
 
-    @KafkaListener(topics = "xxxxx", groupId = "${spring.kafka.consumer.group-id}")
+    @KafkaListener(topics = "${app.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(String message) {
         log.info("Message receive : {}", message);
     }
